06f2c7ced1f1366426517aae6023b317dc10aa98
Tim <79192020+loowtime@users.noreply.github.com>
2021-08-29 01:23:41 +0200

    Update vanishCMD.java

--- src/main/java/de/steuer/lobby/vanish/vanishCMD.java
+++ src/main/java/de/steuer/lobby/vanish/vanishCMD.java
@@ -14,12 +14,12 @@ public class vanishCMD implements CommandExecutor
         if (args.length == 1) {
             final Player player = Bukkit.getPlayer(args[0]);
             if (player == null) {
-                sender.sendMessage(prefix + ChatColor.RED + "Der Spieler ist nicht online");
+                sender.sendMessage(prefix + ChatColor.RED + "Der Spieler ist nicht online!");
                 return true;
             }
             if (vanishManager.isVanished(player)) {
                 vanishManager.setVanished(player, false);
-                sender.sendMessage(prefix + ChatColor.GREEN + "Der Spieler ist jetzt nicht mehr im Vanish");
+                sender.sendMessage(prefix + ChatColor.RED + "Der Spieler ist jetzt nicht mehr im Vanish!");
             }
             else {
                 vanishManager.setVanished(player, true);
@@ -32,11 +32,11 @@ public class vanishCMD implements CommandExecutor
                 final Player player = (Player)sender;
                 if (vanishManager.isVanished(player)) {
                     vanishManager.setVanished(player, false);
-                    sender.sendMessage(prefix + ChatColor.GREEN + "Du bist jetzt nicht mehr im Vanish");
+                    sender.sendMessage(prefix + ChatColor.RED + "Du bist jetzt nicht mehr im Vanish!");
                 }
                 else {
                     vanishManager.setVanished(player, true);
-                    sender.sendMessage(prefix + ChatColor.GREEN + "Du bist jetzt im Vanish");
+                    sender.sendMessage(prefix + ChatColor.GREEN + "Du bist jetzt im Vanish!");
                 }
                 return true;
             }

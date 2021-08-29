1c26cd466161a18d3f9b1c55e6d67365f4173e87
Tim <79192020+loowtime@users.noreply.github.com>
2021-08-29 13:22:00 +0200

    Update InteractListener.java

--- src/main/java/de/universenetwork/lobby/listener/InteractListener.java
+++ src/main/java/de/universenetwork/lobby/listener/InteractListener.java
@@ -109,8 +109,7 @@ public class InteractListener implements Listener {
                     p.openInventory(inv);
                 }
             } else if (e.getItem().getItemMeta().getDisplayName().equalsIgnoreCase("§b§lProfil")) {
-                if (e.getItem().getType() == WOODEN_SWORD) {
-                    p.sendTitle(Main.prefix + "", "§aDas Schwert, §7kommt aus den §bMinen von Bergninen.");
+                if (e.getItem().getType() == WOODEN_AXE) {
                 }
             }
     }
